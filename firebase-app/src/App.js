7b70a37c60ed14834332949903dd1994ba92dfe5
Darius Amenuvor <daarius24@gmail.com>
2025-07-12 14:44:27 -0500

    Basic task handler componet as well as adding changes to app.js

--- firebase-app/src/App.js
+++ firebase-app/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AuthForm from './components/AuthForm';
+import TaskManager from './components/TaskManager';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+    return unsub;
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 20 }}>
+      <h1>🚀 Firebase React App</h1>
+      {user ? (
+        <>
+          <p>Welcome, {user.email}!</p>
+          <button onClick={() => auth.signOut()}>Logout</button>
+          <TaskManager />
+        </>
+      ) : (
+        <AuthForm />
+      )}
     </div>
   );
 }
